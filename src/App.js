4be721eef9dbb10c76e6a70458938f19577e9bcf
Marccc <129010043+Marcc88@users.noreply.github.com>
2024-12-16 18:04:58 +0200

    maaaac

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
-import Home from './Pages/Home'
-import Doctors from './Pages/Doctors'
-import Login from './Pages/Login'
-import SignUp from './Pages/SignUp';
-import About from './Pages/About'
-import Contact from './Pages/Contact'
-import MyProfile from './Pages/MyProfile'
-import MyAppointment from './Pages/MyAppointment'
-import Appointment from './Pages/Appointment'
-import Footer from './Components/Footer/Footer'
-import Home from './Pages/Home/Home';
+// import Home from './Pages/Home'
+import Doctors from '../src/Pages/Doctors/Doctors'
+import Login from '../src/Pages/Login/Login'
+import SignUp from '../src/Pages/Sign Up/SignUp';
+import About from '../src/Pages/About/About'
+import Contact from '../src/Pages/Contact/Contact'
+import MyProfile from '../src/Pages/My Profile/MyProfile'
+import MyAppointment from '../src/Pages/My Appointment/MyAppointment'
+import Appointment from '../src/Pages/Appointmrnt/Appointment'
+import Footer from '../src/Components/Footer/Footer.jsx'
+import Home from '../src/Pages/Home/Home';
 // import {Filter} from '../src/Pages/Doctors/Filter.jsx'
 function App() {
   return (
