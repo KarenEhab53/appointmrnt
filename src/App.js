61866ae4757df3fd40615bdcf08b6e7ee9fa6e0b
Marccc <129010043+Marcc88@users.noreply.github.com>
2024-12-16 16:38:39 +0200

    marks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import {  Route, Routes } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
-import Home from './Pages/Home'
-import Doctors from './Pages/Doctors'
-import Login from './Pages/Login'
-import SignUp from './Pages/SignUp';
-import About from './Pages/About'
-import Contact from './Pages/Contact'
-import MyProfile from './Pages/MyProfile'
-import MyAppointment from './Pages/MyAppointment'
-import Appointment from './Pages/Appointment'
+// import Home from '../src/Pages/Home'
+import Doctors from '../src/Pages/Doctors/Doctors.jsx'
+import Login from '../src/Pages/Login/Login';
+import SignUp from '../src/Pages/Sign Up/SignUp';
+import About from '../src/Pages/About/About'
+import Contact from '../src/Pages/Contact/Contact'
+import MyProfile from '../src/Pages/My Profile/MyProfile'
+import MyAppointment from '../src/Pages/My Appointment/MyAppointment'
+import Appointment from '../src/Pages/Appointmrnt/Appointment'
 import Footer from './Components/Footer/Footer'
+import Home from './Pages/Home/Home';
+// import {Filter} from '../src/Pages/Doctors/Filter.jsx'
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-     <Routes>
-      
+      {/* <NavBar/> */}
+     {/* <Routes>
      <Route path='/' element={<Home/>}/>
 <Route path='/doctors' element={<Doctors/>}/>
 <Route path='/doctors/:speciality' element={<Doctors/>}/>
@@ -26,8 +27,11 @@ function App() {
 <Route path='/my-profile' element={<MyProfile/>}/>
 <Route path='/my-appointment' element={<MyAppointment/>}/>
 <Route path='/my-appointment/:docId' element={<Appointment/>}/>
-     </Routes>
-     <Footer/>
+     </Routes> */}
+<Doctors/>
+<Footer/>
+{/* <Filter/> */}
+
     </div>
   );
 }
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
-import './footer.css'
+import React from 'react';
+import './footer.css';
+import Prespecto from '../../Images/InsiderStats.png';
+import logo from '../../Images/Vector (5).png';
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+    <footer className="footer">
+      <div className="footer-container">
+        {/* Left Section */}
+        <div className="footer-left">
+          <div className="footer-brand">
+            <img src={logo} alt="Image 2" className="footer-image" />
+            <img src={Prespecto} alt="Logo" className="logoPhoto" />
+          </div>
+          <p className="footer-description">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry’s standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
 
-export default Footer
+        {/* Center Section */}
+        <div className="footer-center">
+          <h2>COMPANY</h2>
+          <ul className="footer-links">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        {/* Right Section */}
+        <div className="footer-right">
+          <h2>GET IN TOUCH</h2>
+          <p>+1-212-456-7890</p>
+          <p>greatstackdev@gmail.com</p>
+          <div className="footer-images">
+            {/* <img src={Prespecto} alt="Image 1" className="footer-image" /> */}
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        Copyright © 2024 GreatStack - All Rights Reserved.
+      </div>
+    </footer>
+
+    </>
+  );
+};
+
+export default Footer;
